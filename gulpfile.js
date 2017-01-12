75fa268b74e7062ba75f722894c8b3a11f8349f0
José Carlos de Camargo <jccamargo15@gmail.com>
2017-01-12 01:11:28 -0200

    fim da atividade

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var htmlmin = require('gulp-htmlmin');
+
+gulp.task("compilar-css", function(){
+	return gulp.src("./source/scss/*.scss")
+	.pipe(sass())
+	.pipe(gulp.dest("./dist/css"))
+});
+
+gulp.task("minify-html", function() {
+  return gulp.src("./source/*.html")
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest("./dist"));
+});
+
+gulp.task("escuta-me", function(){
+	gulp.watch("./source/scss/*.scss", ["compilar-css"]);
+	gulp.watch("./source/*.html", ["minify-html"]);
+});
